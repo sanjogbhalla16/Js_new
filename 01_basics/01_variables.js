2154381b593ac7b1f7349de12cee997218c68bcc
SanjogBhalla_16 <70087319+sanjogbhalla16@users.noreply.github.com>
2023-11-24 07:33:32 +0000

    we have learnt about console.table([])

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -6,6 +6,10 @@ let accountStatus;
 
 // accountId = 2; // not allowed
 
+/*
+ we dont use var now kyuki woh issue deti hai functional scope aur block scope mai  
+*/
+
 accountEmail = "sb@gmail.com";
 accountPassword = "123";
 accountCity = "Gurgaon";
