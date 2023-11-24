b554fb5c9460bde741dc1ca0c430ee532ef407fd
SanjogBhalla_16 <70087319+sanjogbhalla16@users.noreply.github.com>
2023-11-24 07:25:18 +0000

    we have seen console.table

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553;
+let accountEmail = "sboy75065@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jabalpur";
+let accountStatus;
+
+// accountId = 2; // not allowed
+
+accountEmail = "sb@gmail.com";
+accountPassword = "123";
+accountCity = "Gurgaon";
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountStatus]);
