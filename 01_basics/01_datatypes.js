30e5ec6b43f67d794d12f29b48bd87beb004c564
SanjogBhalla_16 <70087319+sanjogbhalla16@users.noreply.github.com>
2023-11-25 17:38:50 +0000

    learned about undefined and null

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; //treat all Js code as newer version
+
+//alert(3+3) // we are using nodejs not brower
+
+console.log(3 +
+    3); // not to use like this , code readibility should be high
+
+console.log("sanjog");
+
+let name = "sanjog";
+let age = 18;
+let isLoggedIn = false;
+let state = null;
+
+//data types
+//number
+//BigInt
+//string = ""
+//boolean => true/false
+//null => stand alone value
+//undefined => no value is assigned
+//symbol => used most in react to find the uniqueness
+
+
+//objecct
+console.log(typeof(null)); // object
+console.log(typeof(undefined)); // undefined
+
+
+
+
+    
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Js_new
 Full and final Javascript code repo , santra khate hue :)
+
+# Documentations jisse maine reference lena hai 
+https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+https://tc39.es/ecma262/
+
