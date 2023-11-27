cbe1e813673ad7e2bd95c78a355e0bbf9bf64a49
SanjogBhalla_16 <70087319+sanjogbhalla16@users.noreply.github.com>
2023-11-27 17:34:43 +0000

    here we have seen comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2" > 1); //true
+// console.log("02" > 1); //true
+
+// console.log(null > 0); //false
+// console.log(null == 0); //false
+// console.log(null >= 0); // true
+
+//the comparison >= and == are two different , comparison will convert null into a number that is 0
+
+// console.log(undefined > 0); //false
+// console.log(undefined < 0); //false
+// console.log(undefined == 0); //false
+
+// === it will use a stict check
+
+console.log("2" === 2); // false // this will also check the datatype
