c8d22394c13660b95f70cdf8d136cc8dda399930
SanjogBhalla_16 <70087319+sanjogbhalla16@users.noreply.github.com>
2023-12-01 09:13:23 +0000

    learned about hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,9 +1,46 @@
 let a = 300;
-if(true){
+if (true) {
     let a = 10;
     const b = 20;
-    console.log("INNER: ",a);
+    // console.log("INNER: ", a);
 }
-console.log(a);
-// console.log(b);
-// console.log(c);
+// console.log(a);
+// // console.log(b);
+// // console.log(c);
+
+function one() {
+    const username = "hitesh";
+
+    function two() {
+        const website = "youtube";
+        console.log(username);
+    }
+    // console.lo(website);
+    // two();
+}
+// one();
+
+if (true) {
+    const username = "hitesh";
+    if (username === "hitesh") {
+        const website = "youtube";
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+/*****************interesting*******************/
+console.log(addone(3));
+
+function addone(num) {
+    return num + 1;
+}
+
+addTwo(2); // this will show error this is called hoisting
+
+const addTwo = function (num) { // this is an expression
+    return num + 2;
+}
+
+// addTwo(2);//this will not show error 
