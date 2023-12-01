01caf021c1c77ca348e5729d8f853962c72b37d8
SanjogBhalla_16 <70087319+sanjogbhalla16@users.noreply.github.com>
2023-12-01 07:36:29 +0000

    functions and advance topics

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -34,3 +34,33 @@ function loginUserMessage(username = "sam"){
 }
 console.log(loginUserMessage("hitesh"));
 console.log(loginUserMessage()); // undefined
+
+
+function calculateCartPrice(val1,val2,...num1){ // spread operator and rest operator are same
+    return num1;
+}
+console.log(calculateCartPrice(200,300,400,500));
+
+//insert object in a function
+
+const user = {
+    name: "sanjog",
+    price: 999
+}
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.name} and price is ${anyObject.price}`);
+}
+
+// handleObject(user); // if the value is not correct it will show undefined
+handleObject({
+  name:"sam",
+  price: 399
+});
+
+const myNewArray = [200,400,100,600];
+
+function returnSecondValue(getArray){
+    return getArray[1];
+}
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,500,100,600]));
